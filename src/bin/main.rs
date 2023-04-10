33a3d92d0324847d4fb61a3bcbc54cfad9b077d8
oraqlle <41113853+oraqlle@users.noreply.github.com>
2023-04-10 13:55:40 +1000

    Added basic CLI with prompt and history

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello Lix");
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
+
+fn main() -> Result<()> {
+    let mut editor = DefaultEditor::new()?;
+
+    #[cfg(feature = "with-file-history")]
+    if editor.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
+    println!("Lix v0.0.1");
+    println!("Press CTRL-C to exit\n");
+
+    loop {
+        let readline = editor.readline("lix > ");
+
+        match readline {
+            Ok(line) => {
+                match editor.add_history_entry(line.as_str()) {
+                    Ok(_) => println!("Line: {}", line),
+                    Err(err) => {
+                        println!("Error adding line: {}, to the history.", line);
+                        println!("Aborting with error: {}", err);
+                        break
+                    }
+                }
+                
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+
+    #[cfg(feature = "with-file-history")]
+    editor.save_history("history.txt");
+    Ok(())
+
 }
--- src/lib/types/mod.rs
+++
@@ -1,7 +0,0 @@
-mod env;
-mod func;
-mod r#type;
-
-pub use env::*;
-pub use func::*;
-pub use r#type::*;
